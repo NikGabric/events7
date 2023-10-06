c425295bc717c2850d2d9ed2ff39206ec2f3c61b
NikGabric <36707304+NikGabric@users.noreply.github.com>
2023-10-06 11:46:15 +0200

    backend - delete event implemented

--- backend/src/event/event.controller.ts
+++ backend/src/event/event.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { EventService } from './event.service';
 import { Event } from 'src/model/event.entity';
 import { EventDto } from './dtos/event.dto';
 import { EventInfoDto } from './dtos/event-info.dto';
+import { MessageDto } from 'src/common/dtos/message.dto';
 
 @Controller('event')
 export class EventController {
@@ -16,5 +25,13 @@ export class EventController {
   @Post()
   postEvent(@Body() eventDto: EventDto): Promise<EventInfoDto> {
     return this.eventService.postEvent(eventDto);
+  }
+
+  @Delete()
+  deleteEvent(
+    @Query('id', new ParseIntPipe()) id: number,
+  ): Promise<MessageDto> {
+    console.log(id);
+    return this.eventService.deleteEvent(id);
   }
 }
--- backend/src/event/event.service.ts
+++ backend/src/event/event.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EventRepository } from 'src/model/event.repository';
 import { Event } from 'src/model/event.entity';
 import { EventDto } from './dtos/event.dto';
 import { EventInfoDto } from './dtos/event-info.dto';
+import { MessageDto } from 'src/common/dtos/message.dto';
 
 @Injectable()
 export class EventService {
@@ -19,5 +20,13 @@ export class EventService {
   async postEvent(eventDto: EventDto): Promise<EventInfoDto> {
     const event = await this.eventRepository.save(eventDto);
     return new EventInfoDto(event);
+  }
+
+  async deleteEvent(id: number): Promise<MessageDto> {
+    console.log(id);
+    const res = await this.eventRepository.delete(id);
+    if (res.affected !== 0) {
+      return new MessageDto('Event deleted successfully');
+    } else throw new NotFoundException();
   }
 }
