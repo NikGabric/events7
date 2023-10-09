d88fc8b3de32f99252ba1325a617d5d7fc420b12
NikGabric <36707304+NikGabric@users.noreply.github.com>
2023-10-09 13:17:06 +0200

    backend - tests for the auth middleware

--- backend/src/middleware/auth.middleware.spec.ts
+++ backend/src/middleware/auth.middleware.spec.ts
@@ -1,7 +1,80 @@
 import { AuthMiddleware } from './auth.middleware';
+import { NextFunction, Request, Response } from 'express';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
 describe('AuthMiddleware', () => {
-  it('should be defined', () => {
-    expect(new AuthMiddleware()).toBeDefined();
+  let authMiddleware: AuthMiddleware;
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+  let mockAxios: MockAdapter;
+
+  beforeEach(() => {
+    authMiddleware = new AuthMiddleware();
+    req = {} as Partial<Request>;
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as Partial<Response>;
+    next = jest.fn();
+
+    mockAxios = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    mockAxios.restore();
+  });
+
+  it('should delete countryCode when type is not "ads"', async () => {
+    req.body = { type: 'other', countryCode: 'US' };
+
+    await authMiddleware.use(req as Request, res as Response, next);
+
+    expect(req.body.countryCode).toBeUndefined();
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should handle successful external service response', async () => {
+    req.body = { type: 'ads', countryCode: 'US' };
+
+    mockAxios
+      .onGet('https://us-central1-o7tools.cloudfunctions.net/fun7-ad-partner')
+      .reply(200, {
+        ads: 'sure, why not!',
+      });
+
+    await authMiddleware.use(req as Request, res as Response, next);
+
+    expect(req.body.countryCode).toBeUndefined();
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should handle unsuccessful external service response', async () => {
+    req.body = { type: 'ads', countryCode: 'US' };
+
+    mockAxios
+      .onGet('https://us-central1-o7tools.cloudfunctions.net/fun7-ad-partner')
+      .reply(200, { ads: 'you shall not pass!' });
+
+    await authMiddleware.use(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(403);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Access Forbidden' });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  it('should handle external service error', async () => {
+    req.body = { type: 'ads', countryCode: 'US' };
+
+    mockAxios
+      .onGet('https://us-central1-o7tools.cloudfunctions.net/fun7-ad-partner')
+      .networkError();
+
+    await authMiddleware.use(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Internal Server Error' });
+    expect(next).not.toHaveBeenCalled();
   });
 });
--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,38 +1,38 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-    const { type } = req.body;
+    const { type, countryCode } = req.body;
     if (type != 'ads') {
       delete req.body.countryCode;
-      next();
-      return;
+      return next();
     }
     try {
+      const axiosConfing: AxiosRequestConfig = {
+        params: {
+          countryCode,
+        },
+        auth: {
+          username: process.env.AUTH_FUN7_USER,
+          password: process.env.AUTH_FUN7_PASS,
+        },
+      };
       const response = await axios.get(
         'https://us-central1-o7tools.cloudfunctions.net/fun7-ad-partner',
-        {
-          params: {
-            ...req.body,
-          },
-          auth: {
-            username: process.env.AUTH_FUN7_USER,
-            password: process.env.AUTH_FUN7_PASS,
-          },
-        },
+        axiosConfing,
       );
 
       if (response.status === 200 && response.data.ads === 'sure, why not!') {
         delete req.body.countryCode;
-        next();
+        return next();
       } else {
-        res.status(403).json({ message: 'Access Forbidden' });
+        return res.status(403).json({ message: 'Access Forbidden' });
       }
     } catch (err: any) {
-      res.status(500).json({ message: 'Internal Server Error' });
+      return res.status(500).json({ message: 'Internal Server Error' });
     }
   }
 }
